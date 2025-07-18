d0d136dc3d8598d01343a75e23ee02875139de09
Sylky <miiyusli@gmail.com>
2025-07-18 14:13:30 +1200

    add lighting to shop scene

--- scripts/ui/Cauldron/CauldronAttributesText.cs
+++ scripts/ui/Cauldron/CauldronAttributesText.cs
@@ -3,12 +3,21 @@ using Godot;
 public partial class CauldronAttributesText : RichTextLabel
 {
     [Export] private CauldronItemList _cauldronItemList;
+    [Export] private CompressedTexture2D _earthIcon;
+    [Export] private CompressedTexture2D _waterIcon;
+    [Export] private CompressedTexture2D _airIcon;
+    [Export] private CompressedTexture2D _fireIcon;
+    [Export] private CompressedTexture2D _darkIcon;
 
     public override void _Ready() => Clear();
 
     public void OnCauldronUpdated()
     {
         var attr = _cauldronItemList.GetTotalAttributes();
-        Text = $"E: {attr.Earth}\nW: {attr.Water}\nA: {attr.Air}\nF: {attr.Fire}\nD: {attr.Dark}";
+        Text = $"[img=48x48]{_earthIcon.ResourcePath}[/img]: {attr.Earth}\n"
+             + $"[img=48x48]{_waterIcon.ResourcePath}[/img]: {attr.Water}\n"
+             + $"[img=48x48]{_airIcon.ResourcePath}[/img]: {attr.Air}\n"
+             + $"[img=48x48]{_fireIcon.ResourcePath}[/img]: {attr.Fire}\n"
+             + $"[img=48x48]{_darkIcon.ResourcePath}[/img]: {attr.Dark}";
     }
 }
